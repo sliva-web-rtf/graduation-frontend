b94a2832acec9c8192c35200bde209c49a841fd7
Egor Ryazanov <egor.ryazanov@saritasa.com>
2024-04-29 02:29:26 +0500

    Add onboarding sidebar

--- src/pages/OnboardingPage/ui/OnboardingPage.tsx
+++ src/pages/OnboardingPage/ui/OnboardingPage.tsx
@@ -1,9 +1,21 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { memo } from 'react';
 
+import { OnboardingSidebar } from 'widgets/OnboardingSidebar';
+import classNames from './OnboardingPage.module.scss';
+
 const OnboardingPage = memo(() => (
-    <Box display="flex" sx={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        <Typography variant="h2">Onboarding</Typography>
+    <Box className={classNames.container}>
+        <OnboardingSidebar />
+        <Stack spacing={3} className={classNames.mainContainer}>
+            <Typography variant="h2" fontWeight={700}>
+                Заполните данные профиля
+            </Typography>
+            <Typography variant="body2" className={classNames.description}>
+                Вводимые данные обеспечат более эффективную коммуникацию, а также обеспечат персональную выдачу, которая
+                будет максимально соответствовать Вашим запросам.
+            </Typography>
+        </Stack>
     </Box>
 ));
 
