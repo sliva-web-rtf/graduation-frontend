aaeddcca9e9d554b26b8aca66d721d10bcb06527
nchertolin <nchertolin@gmail.com>
2024-11-15 23:02:40 +0500

    fix: build error

--- src/shared/ui/HelperText/HelperText.tsx
+++ src/shared/ui/HelperText/HelperText.tsx
@@ -1,17 +1,22 @@
-/* eslint-disable react/prop-types */
-import { Typography } from '@mui/material';
 import CancelIcon from '@mui/icons-material/Cancel';
+import { Typography } from '@mui/material';
 import { FieldError } from 'react-hook-form';
 
 interface HelperTextProps {
     error?: FieldError;
 }
-export const HelperText: React.FC<HelperTextProps> = ({ error }) =>
-    error && (
+
+export const HelperText = (props: HelperTextProps) => {
+    const { error } = props;
+
+    if (!error) {
+        return null;
+    }
+
+    return (
         <Typography display="flex" alignItems="center" variant="bodyXS" gap={0.5}>
             <CancelIcon fontSize="inherit" />
             {error.message}
         </Typography>
     );
-
-export default HelperText;
+};
