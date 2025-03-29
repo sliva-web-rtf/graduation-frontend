90a11e4c74f6152500895193af02e52714b4dafe
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-03-29 07:57:57 +0500

    fix: correct import and style PersonSummary

--- src/entities/Person/index.ts
+++ src/entities/Person/index.ts
@@ -1,4 +1,4 @@
 export { PersonCard } from './ui/PersonCard';
 export { PersonCardSkeleton } from './ui/PersonCard';
 export { useGetPersonQuery } from './api/personApi';
-export { PersonSummary } from './ui/PersonSummary/ui/PersonSummary';
+export { PersonSummary } from './ui/PersonSummary';
--- src/entities/Person/ui/PersonSummary/ui/PersonSummary.tsx
+++ src/entities/Person/ui/PersonSummary/ui/PersonSummary.tsx
@@ -1,14 +1,14 @@
-import { Link, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import { getInitials } from '@/shared/lib/helpers/getInitials';
-import classes from './PersonSummary.module.scss';
 
 interface PersonSummaryProps {
     readonly id: string;
     readonly firstName: string;
     readonly lastName: string;
     readonly isLink: boolean;
+
     readonly degree?: string;
     readonly patronymic?: string;
 }
@@ -23,13 +23,11 @@ export const PersonSummary: FC<PersonSummaryProps> = (props) => {
     return (
         <Stack>
             {isLink ? (
-                <Typography className={classes.name} component={NavLink} to="/" variant="h4">
+                <Typography component={NavLink} to="/" color="primary" variant="h4">
                     {name}
                 </Typography>
             ) : (
-                <Typography className={classes.name} variant="subtitle1">
-                    {name}
-                </Typography>
+                <Typography variant="subtitle1">{name}</Typography>
             )}
 
             <Typography variant="bodyXS" color="secondary">
