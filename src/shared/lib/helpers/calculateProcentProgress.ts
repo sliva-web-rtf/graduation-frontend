6010163895d6c761c305d18205c1fb22ccb520ae
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-04-13 19:41:21 +0500

    refactor: delete comments

--- src/shared/lib/helpers/calculateProcentProgress.ts
+++ src/shared/lib/helpers/calculateProcentProgress.ts
@@ -1,5 +1,10 @@
-export function calculateProcentProgress(datesFromBackend: { title: string; start: string; end: string }[]) {
-    // const curDate = new Date().toISOString()
+interface DataDto {
+    title: string;
+    start: string;
+    end: string;
+}
+
+export function calculateProcentProgress(datesFromBackend: DataDto[]) {
     const curDate = '2025-01-15T11:19:20.310Z';
 
     const procentEveryStage = 100 / datesFromBackend.length;
@@ -7,8 +12,6 @@ export function calculateProcentProgress(datesFromBackend: { title: string; star
     const suitableIndexOfObj = datesFromBackend.findIndex(
         (el) => Date.parse(el.start) <= Date.parse(curDate) && Date.parse(curDate) <= Date.parse(el.end),
     );
-
-    // console.log(datesFromBackend[suitableIndexOfObj]);
 
     const startDay = +datesFromBackend[suitableIndexOfObj].start.substring(8, 10);
     const finishDay = +datesFromBackend[suitableIndexOfObj].end.substring(8, 10);
@@ -18,14 +21,8 @@ export function calculateProcentProgress(datesFromBackend: { title: string; star
     const generalCountDaysInPeriod = finishDay - startDay + 1;
     const procentIntoStage = (pastDays * 100) / generalCountDaysInPeriod;
 
-    //   console.log("Прошедших дней с начала этапа - ", pastDays)
-    //   console.log("Сколько вообще в этапе дней - ", generalCountDaysInPeriod)
-    //   console.log("Прогресс относительно этапа - ", procentIntoStage)
-
     const procentIntoAllLinearProgress =
         procentIntoStage / datesFromBackend.length + procentEveryStage * suitableIndexOfObj;
-
-    // console.log('Процент от общего числа - ', procentIntoAllLinearProgress);
 
     return procentIntoAllLinearProgress;
 }
