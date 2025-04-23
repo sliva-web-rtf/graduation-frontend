128da20eba0b7cfd88599949d60eeceae2646352
nchertolin <nchertolin@gmail.com>
2025-04-23 15:21:54 +0500

    fix: filter by commission in datagrid

--- src/entities/Comission/lib/mapCommissionsDtoToModel.tsx
+++ src/entities/Comission/lib/mapCommissionsDtoToModel.tsx
@@ -1,12 +1,15 @@
 import { CommissionNamesDto, CommissionNamesModel, CommissionsDto, CommissionsModel } from '../model';
 
 export const mapCommissionNamesDtoToModel = (dto: CommissionNamesDto): CommissionNamesModel =>
-    dto.commissions.map((item) => item.name);
+    dto.commissions.map((item) => ({
+        label: `${item.name} (${item.secretaryName})`,
+        value: item.name,
+    }));
 
 export const mapCommissionsDtoToModel = (dto: CommissionsDto): CommissionsModel =>
     dto.commissions.map((item) => ({
         id: item.id,
         number: item.number,
         name: item.name,
-        secretary: item.secretary,
+        secretaryName: item.secretaryName,
     }));
--- src/entities/Comission/model/types/index.ts
+++ src/entities/Comission/model/types/index.ts
@@ -2,16 +2,16 @@ export type CommissionModel = {
     id: string;
     number: number;
     name: string;
-    secretary: string;
+    secretaryName: string;
 };
 
 export type CommissionRequest = {
     id: 'string';
 };
 export type CommissionNamesDto = {
-    commissions: { name: string }[];
+    commissions: Pick<CommissionModel, 'name' | 'secretaryName'>[];
 };
-export type CommissionNamesModel = string[];
+export type CommissionNamesModel = { label: CommissionModel['name']; value: CommissionModel['name'] }[];
 
 export type CommissionsDto = {
     commissions: CommissionModel[];
